ed9e549ed0b0b7cd461b4fe2df67064999dfe821
Shamilysham <137861941+Shamilysham@users.noreply.github.com>
2023-07-20 20:04:19 +0530

    Add files via upload

---
+++ ValidateUrl.java
@@ -0,0 +1,39 @@
+public class ValidateUrl {
+	static String validateurl(String url) {
+		//getting the entered url
+	String enteredUrl=url.substring(0,5);
+	
+	   //Condition for checking schemes
+	switch(enteredUrl) {
+	case "https":
+	System.out.println("valid");
+			
+	case "http":
+		System.out.println("valid");
+		
+	case "ftp":
+		System.out.println("valid");
+		
+	default:
+		System.out.println("Invalid");
+	
+	//Splitting the url
+	String [] splitArray=url.split("?");
+	
+	//will check whether only 2 parameters are passed
+	if(splitArray.length==2) {
+		System.out.println("valid");
+	}
+	
+	else {
+		System.out.println("invalid");
+	}
+	return url;
+	}
+	}
+	public static void main(String[] args) {
+		validateurl("https://www.geeksforgeeks.org/");
+
+	}
+
+}
